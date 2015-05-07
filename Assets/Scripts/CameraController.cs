d2c3148206b57eec3d4269f0d910aee84664d2b7
Filipe Relvas <Filipe@Filipes-MacBook-Pro.local>
2015-05-07 16:04:15 +0100

    Simple prototype, with rigid bodies and forces

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Position of cursor when mouse dragging starts
+	private Vector3 mouseOrigin;
+
+	// Zoom properties
+	private float minFov = 15f;
+	private float maxFov = 90f;
+	private float sensitivity = 5f;
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate ()
+	{
+		// Zoom in/out handling
+		float fov = GetComponent<Camera>().fieldOfView;
+
+		fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+
+		GetComponent<Camera>().fieldOfView = fov;
+
+		// Follow player position
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerSphereController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSphereController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3 (moveVertical, 0.0f, -moveHorizontal);
+		
+		rb.AddForce(movement * speed);
+	}
+}
