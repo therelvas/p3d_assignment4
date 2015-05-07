4d4196884013140b1e1b18ffc0e1e946a1b0604e
Filipe Relvas <Filipe@Filipes-MacBook-Pro.local>
2015-05-07 18:48:52 +0100

    Added moving plataform object and script

---
+++ Assets/Scripts/RotativeFloorController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotativeFloorController : MonoBehaviour {
+	
+	public float rotationSpeed = 80.0f;
+
+	private Rigidbody rb = null;
+	private Vector3 eulerAngleVelocity;
+
+	// Initilization
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		eulerAngleVelocity = new Vector3(0.0f, rotationSpeed, 0.0f);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+		rb.MoveRotation(rb.rotation * deltaRotation);
+	}
+}
