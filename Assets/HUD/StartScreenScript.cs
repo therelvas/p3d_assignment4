36f6c333884cc990796fb51ac499a323ab11bc56
b2rosado <bito_rosado@hotmail.com>
2015-05-21 17:51:18 +0100

    Added StartScreen

---
+++ Assets/HUD/StartScreenScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+	public Button playBtn;
+
+	// Use this for initialization
+	void Start () {
+		playBtn = playBtn.GetComponent<Button> ();
+	}
+	
+	public void StartPressed (){
+		Application.LoadLevel (1);
+	}
+}
