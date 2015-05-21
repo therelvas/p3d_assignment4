a6c4c86313d630a37335fc99a13746ea46ab71fc
b2rosado <bito_rosado@hotmail.com>
2015-05-21 19:11:37 +0100

    StartScreen improved

--- Assets/HUD/StartScreenScript.cs
+++ Assets/HUD/StartScreenScript.cs
@@ -4,13 +4,28 @@ using System.Collections;
 
 public class StartScreenScript : MonoBehaviour {
 	public Button playBtn;
+    public Canvas quitMenu;
 
 	// Use this for initialization
 	void Start () {
 		playBtn = playBtn.GetComponent<Button> ();
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        quitMenu.enabled = false;
 	}
 	
 	public void StartPressed (){
 		Application.LoadLevel (1);
 	}
+
+    public void QuitPressed(){
+        quitMenu.enabled = true;
+    }
+
+    public void NoPressed(){
+        quitMenu.enabled = false;
+    }
+
+    public void YesPressed(){
+        Application.Quit();
+    }
 }
