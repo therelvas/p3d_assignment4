932148c0977e019d8613cb68c18feffb09bdb6cd
Vasco Pires <vascopir@gmail.com>
2015-05-24 21:14:57 +0100

    Diamonds

---
+++ Assets/Scripts/Diamond.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Diamond : MonoBehaviour {
+
+	public AudioClip diamondGrab;
+
+	private GameObject sphere;
+
+	void Awake() {
+		sphere = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if(other.gameObject == sphere) {
+			AudioSource.PlayClipAtPoint(diamondGrab, transform.position);
+			//ADD SCORE
+			Destroy(gameObject);
+		}
+	}
+}
