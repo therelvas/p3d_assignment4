df9ac8f2c4230338a3dc64721f5eda003130e631
Vasco Pires <vascopir@gmail.com>
2015-05-24 04:12:08 +0100

    bundas e bucetas

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	private GameObject sphere;
+
+	void Start() {
+		sphere = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update() {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+	}
+
+}
---
+++ Assets/Scripts/NavRamp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavRamp : MonoBehaviour {
+
+	private int dir = 1;
+	private float speed = 0.5f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * dir * speed);
+		if(transform.position.x >11.5f)
+			dir = -1;
+		if(transform.position.x <10.5f)
+			dir=1;
+
+	}		    
+
+}
