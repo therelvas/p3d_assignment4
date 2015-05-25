c0ee13f3115adf145caea768fa3d3d74aafe3c8a
Vasco Pires <vascopir@gmail.com>
2015-05-26 00:44:32 +0100

    LightOn/Off

---
+++ Assets/Scripts/TurnLightsOn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnLightsOn : MonoBehaviour {
+
+	public GameObject[] l;
+	public GameObject sphere;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.Equals(sphere)) {
+			foreach (GameObject light in l)
+				light.transform.GetComponent<Light>().enabled = true;
+		}
+	}
+}
