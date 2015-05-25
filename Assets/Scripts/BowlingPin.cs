22906a933403c9783cde12eaa23b94ab1cb6cd8f
Vasco Pires <vascopir@gmail.com>
2015-05-25 02:02:11 +0100

    Bowling++

---
+++ Assets/Scripts/BowlingPin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BowlingPin : MonoBehaviour {
+
+	public Text score;
+	private GameObject sphere;
+	private bool once = true;
+	
+	void Awake() {
+		sphere = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if(once)
+			if(other.gameObject == sphere) {
+			Debug.Log("AQUII");
+				once = false;
+				//ADD SCORE
+				Globals.score += 10;
+				if(Globals.score<1000)
+					score.text = (0).ToString()+(Globals.score).ToString();
+				else
+					score.text = (Globals.score).ToString();
+			}
+	}
+
+}
---
+++ Assets/Scripts/NavRampUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavRampUp : MonoBehaviour {
+
+	private int dir = 1;
+	private float speed = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * dir * speed);
+		if(transform.position.y >11f)
+			dir = -1;
+		if(transform.position.y <8.3f)
+			dir=1;
+	}	
+}
