efa882964cdec46086fdefe7a30787b2c13f631d
b2rosado <bito_rosado@hotmail.com>
2015-05-25 19:30:57 +0100

    EndScreen improvements

--- Assets/HUD/EndScreenScript.cs
+++ Assets/HUD/EndScreenScript.cs
@@ -12,8 +12,22 @@ public class EndScreenScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //leaderboardMenu = leaderboardMenu.GetComponent<Canvas>();
+		ArrayList scores = new ArrayList(System.IO.File.ReadAllLines(@"scores.txt"));
+
+		bool added = false;
+		for(int i=0; i<scores.Count; i++)
+			if(Globals.score > i){
+				scores.Insert(i, Globals.playerName + "\t" + Globals.score);
+				added = true;
+			}
+		if(!added)
+			scores.Add(Globals.playerName + "\t" + Globals.score);
+
+		System.IO.File.WriteAllLines(@"scores.txt", (string[])scores.ToArray(typeof(string)));
 		messageLbl.text = Globals.endMessage;
-		scoresLbl.text = "BITO\t1050";
+		scoresLbl.text = "";
+		foreach(string s in scores)
+			scoresLbl.text += s+"\n";
         leaderboardMenu.enabled = false;
         leaderboardMenu.enabled = true;
         quitMenu.enabled = false;
