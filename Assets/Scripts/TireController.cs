1022038b2600c14f3199c6b3df5ea967e9b980a9
Filipe Relvas <Filipe@Filipes-MacBook-Pro.local>
2015-05-25 22:33:42 +0100

    House keeping

--- Assets/Scripts/TireController.cs
+++ Assets/Scripts/TireController.cs
@@ -3,8 +3,21 @@ using System.Collections;
 
 public class TireController : MonoBehaviour {
 	
-	void OnTriggerEnter(Collider collider) {
-
+	public int thrust;
+	private Rigidbody rb;
+	public Vector3 impulseDirection;
+	
+	public void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	public void FixedUpdate() {
+		//rb.velocity = Vector3.zero;
+		rb.AddForce(impulseDirection * thrust, ForceMode.Impulse);
+	}
+	
+	private void OnTriggerEnter(Collider collider) {
+		
 		if(collider.gameObject.name.Contains("tire_cemetery")) {
 			gameObject.SetActive(false);
 		}
